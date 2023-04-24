d1b48124497e0f709328e024d388d12b5f8958ec
Huynh Tien <huynhqtienvtag@gmail.com>
2023-04-24 20:44:03 +0700

    Initial commit

---
+++ src/main/java/me/hsgamer/gamesinthebox/exampleexpansion/ExampleExpansion.java
@@ -0,0 +1,20 @@
+package me.hsgamer.gamesinthebox.exampleexpansion;
+
+import me.hsgamer.hscore.expansion.common.Expansion;
+
+public class ExampleExpansion implements Expansion {
+    @Override
+    public boolean onLoad() {
+        return true;
+    }
+
+    @Override
+    public void onEnable() {
+        // Do something
+    }
+
+    @Override
+    public void onDisable() {
+        // Do something
+    }
+}
